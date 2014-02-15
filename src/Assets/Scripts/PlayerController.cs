1e0aaa85ae81bc7a101a33434ec083817c300e30
Jørgen Fogh <jrgfogh@gmail.com>
2014-02-15 01:37:29 +0100

    Simple test scene.

---
+++ src/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float Speed;
+
+	void FixedUpdate () {
+		var moveVertical = Input.GetAxis ("Horizontal");
+		var moveHorizontal = Input.GetAxis ("Vertical");
+
+		var movement = new Vector3 (-moveHorizontal, 0, -moveVertical);
+	    gameObject.rigidbody.AddForce (movement * Speed * Time.deltaTime);
+	}
+}
