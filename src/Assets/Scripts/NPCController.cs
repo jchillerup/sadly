29ac397e59abe88a2352442ebab1e72a6714004c
Jens Christian Hillerup <jens hillerup net>
2014-02-15 12:05:36 +0100

    Refinements of the NPC code. Pushes player away

--- src/Assets/Scripts/NPCController.cs
+++ src/Assets/Scripts/NPCController.cs
@@ -7,10 +7,12 @@ public enum NPCState {
 };
 
 public class NPCController : MonoBehaviour {
-	public GameObject player;
+	private static float PrivateSphereThreshold = 1.5f;
 
+	public GameObject player;
 	public NPCState state = NPCState.IDLE;
-	public int happiness = 100;
+	private int happiness = 100;
+	private bool hostile = false;
 
 	float GetDistanceToPlayer() {
 		Vector3 distance = player.transform.position - this.transform.position;
@@ -18,9 +20,41 @@ public class NPCController : MonoBehaviour {
 		return distance.magnitude;
 	}
 
+	void increaseHappiness(int howMuch) {
+		if (!this.hostile) {
+			this.happiness += howMuch;
+		}
+		if (this.happiness > 100) {
+			this.happiness = 100;
+		}
+	}
+	void increaseHappiness() {
+		increaseHappiness (1);
+	}
 
+	void decreaseHappiness(int howMuch) {
+		this.happiness -= howMuch;
+
+		if (this.happiness < 0) {
+			this.happiness = 0;
+		}
+	}
+	void decreaseHappiness() {
+		decreaseHappiness (1);
+	}
+
+	#region UNITY PRIMITIVES
 	void FixedUpdate() {
+		if (GetDistanceToPlayer () < PrivateSphereThreshold) {
+			decreaseHappiness();
+		} else {
+			increaseHappiness();
+		}
 
+		
+		if (this.happiness < 20) {
+			this.hostile = true;
+		}
 	}
 
 	// Use this for initialization
@@ -30,10 +64,20 @@ public class NPCController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (GetDistanceToPlayer () < 2.0f) {
-			Debug.Log("Too close!");
+		if (this.happiness < 80) {
+			this.renderer.material.color = new Color (1.0f, 0.0f, 0.0f);
+		} else {
+			this.renderer.material.color = new Color (1.0f, 1.0f, 1.0f);
+		}
 
-			this.renderer.material.color = new Color(1.0f, 0.0f, 0.0f);
+		if (this.hostile) {
+			this.renderer.material.color = new Color(0.0f, 0.0f, 0.0f);
+
+			// If the NPC is hostile and the player is close, they will try to push her away
+			if (GetDistanceToPlayer() < PrivateSphereThreshold) {
+				this.player.rigidbody.AddForce(1.0f, 0.0f, 0f);
+			}
 		}
 	}
+	#endregion
 }
