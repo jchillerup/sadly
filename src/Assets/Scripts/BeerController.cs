e4843e9dd4b621d7f871e605bcbbab514de9bda4
Jens Christian Hillerup <jens hillerup net>
2014-02-15 20:34:46 +0100

    Beers are now thrown away after drinking

--- src/Assets/Scripts/BeerController.cs
+++ src/Assets/Scripts/BeerController.cs
@@ -2,12 +2,19 @@
 using System.Collections;
 
 public class BeerController : Interactable {
-		
+	bool drank = false;
+
 	public void Start() {
 	}
 
 	public void Interact(PlayerController p) {
-		p.Beverages += 1.0f;
-		Debug.Log ("drank beer");
+		if (!drank) {
+			p.Beverages += 1.0f;
+			Debug.Log ("drank beer");
+			this.drank = true;
+		}
+		Vector3 direction = p.gameObject.transform.forward + new Vector3 (400f, 600f, 400f);
+
+		this.gameObject.rigidbody.AddForce (direction);
 	}
 }
