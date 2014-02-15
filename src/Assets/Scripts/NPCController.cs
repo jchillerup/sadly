1dd3580fded7f38bd54a9ef16a6939c048349d2d
Jens Christian Hillerup <jens hillerup net>
2014-02-15 10:50:14 +0100

    Began working on NPC mechanics

---
+++ src/Assets/Scripts/NPCController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum NPCState {
+	IDLE,
+	CHATTING
+};
+
+public class NPCController : MonoBehaviour {
+	public GameObject player;
+
+	public NPCState state = NPCState.IDLE;
+	public int happiness = 100;
+
+	float GetDistanceToPlayer() {
+		Vector3 distance = player.transform.position - this.transform.position;
+
+		return distance.magnitude;
+	}
+
+
+	void FixedUpdate() {
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GetDistanceToPlayer () < 2.0f) {
+			Debug.Log("Too close!");
+
+			this.renderer.material.color = new Color(1.0f, 0.0f, 0.0f);
+		}
+	}
+}
